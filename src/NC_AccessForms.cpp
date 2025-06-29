80f3b5c09fde97373d31bd934831326f485942c6
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-06-29 18:28:07 +0200

    Add the code.

---
+++ src/NC_AccessForms.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+
+// clang-format off
+#include <netcdf>
+#include "MAT_Matrix.h"
+// clang-format on
+
+
+template<typename T>
+MyMatrix<T> ReadMatrix(std::vector<int16_t> const& V) {
+  int dim = 9;
+  MyMatrix<T> M(dim, dim);
+  size_t pos = 0;
+  for (int u=0; u<dim; u++) {
+    for (int v=0; v<=u; v++) {
+      int16_t val = V[pos];
+      T val_T = UniversalScalarConversion<T,int16_t>(val);
+      M(u, v) = val_T;
+      M(v, u) = val_T;
+      pos += 1;
+    }
+  }
+  bool test = IsPositiveDefinite(M, std::cerr);
+  if (!test) {
+    std::cerr << "The matrix is not positive definite\n";
+    throw TerminalException{1};
+  }
+  return M;
+}
+
+
+
+
+
+void ReadMatrices(std::string const& FileName, size_t& index, size_t n_short) {
+  netCDF::NcFile dataFileI(FileName, netCDF::NcFile::read);
+  std::cerr << "ReadMatrices, FileName=" << FileName << "\n";
+  //
+  netCDF::NcVar var1 = dataFileI.getVar("minQ");
+  size_t number_perfect = var1.getDim(0).getSize();
+  std::vector<int> V_minQ(number_perfect);
+  var1.getVar(V_minQ.data());
+  //
+  size_t dim = 45;
+  netCDF::NcVar var2 = dataFileI.getVar("minQ");
+  std::vector<size_t> count{1, dim};
+  std::vector<size_t> start{0, 0};
+  std::vector<int16_t> V_form(dim);
+  //
+  for (size_t i=0; i<number_perfect; i++) {
+    if (V_minQ[i] == n_short) {
+      start[0] = i;
+      var2.getVar(start, count, V_form.data());
+      MyMatrix<mpz_class> M = ReadMatrix(V_form);
+      std::string MatFile = "Matrix_" + std::to_string(index);
+      WriteMatrixFile(MatFile, M);
+      index += 1;
+      std::cerr << "Now index=" << index << "\n";
+    }
+  }
+
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 4) {
+      std::cerr << "NC_AccessForms [n_short]\n";
+      throw TerminalException{1};
+    }
+    std::string n_short_str = argv[1];
+    size_t n_short = ParseScalar<int>(n_short_str);
+    //
+    // Now reading the
+    //
+    size_t index = 0;
+    size_t n_files = 64;
+    for (size_t i_file=0; i_file<n_files; i_file++) {
+      std::string FileName = "perfect_" + StringNumber(i_file, 4) + ".nc";
+      ReadMatrices(FileName, index, n_short);
+    }
+    std::cerr << "Normal termination of NC_AccessForms\n";
+  } catch (TerminalException const &e) {
+    std::cerr << "Error in NC_ComputeAverage\n";
+    exit(e.eVal);
+  }
+}
